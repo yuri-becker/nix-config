3fe2a4280d4574270b2538f1a5635ea713dfd92b
Yuri Becker <yuri@control.alt.coop>
2025-10-16 18:57:43 +0200

    Fix Hash issue

--- users/yuri/cli/btop.nix
+++ users/yuri/cli/btop.nix
@@ -1,13 +1,9 @@
-{ pkgs, ... }:
+{ ... }:
 let
   theme = builtins.readFile (
     builtins.fetchurl {
-      url = "https://github.com/catppuccin/btop/blob/f437574b600f1c6d932627050b15ff5153b58fa3/themes/catppuccin_mocha.theme";
-      sha256 = # No idea why i get different SHA256s between Mac and Linux
-        if pkgs.stdenv.isDarwin then
-          "0yy441kyqwlpgl3sdv1s4fm2cf72h5q3b6qvqqb4pylw5i0rdgl1"
-        else
-          "13x4ar3l4pr03kcplyvrw641civfii4ypz901y0apwyjxd9ikh1m";
+      url = "https://raw.githubusercontent.com/catppuccin/btop/f437574b600f1c6d932627050b15ff5153b58fa3/themes/catppuccin_mocha.theme";
+      sha256 = "0i263xwkkv8zgr71w13dnq6cv10bkiya7b06yqgjqa6skfmnjx2c";
     }
   );
 in
