9ed9b440c034a831deb7bcbc35648ad0886bf62d
Yuri <hi@yuri.li>
2025-10-19 20:24:26 +0200

    Autotag ASMR files

--- hosts/mantis/services/pinchflat.nix
+++ hosts/mantis/services/pinchflat.nix
@@ -1,10 +1,32 @@
-{ lib, config, ... }:
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
 let
   domain = "yt.home.arpa";
   navidromeDomain = "asmr.home.arpa";
   sopsFile = ./pinchflat.secrets.yaml;
+  tagAsmrFiles =
+    with pkgs;
+    writeScriptBin "tag-asmr-files" ''
+      #!${fish}/bin/fish
+      argparse 'media=' -- $argv; or return 1
+
+      for folder in $_flag_media/*
+          set -l artist (basename $folder)
+          echo "Going through $artist"
+          set -l track 1
+          for file in (ls $folder | sort -r)
+              ${tageditor}/bin/tageditor set album="$artist" albumartist="$artist" track=$track artist="$artist" -f "$folder/$file"
+              set track (math $track + 1)
+          end
+      end
+    '';
 in
 {
+  environment.systemPackages = [ tagAsmrFiles ];
   sops.secrets."pinchflat/secret_key_base".sopsFile = sopsFile;
   sops.templates."pinchflat-secrets.env" = {
     content = lib.generators.toKeyValue { } {
@@ -57,4 +79,20 @@ in
       url = "https://yt.home.arpa";
     }
   ];
+
+  systemd.timers."tag-asmr-files" = {
+    enable = true;
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnCalendar = "daily";
+      Unit = "tag-asmr-files.service";
+    };
+  };
+  systemd.services."tag-asmr-files" = {
+    serviceConfig = {
+      Type = "simple";
+      User = config.services.pinchflat.user;
+      ExecStart = "${tagAsmrFiles}/bin/tag-asmr-files --media ${config.services.pinchflat.mediaDir}";
+    };
+  };
 }
