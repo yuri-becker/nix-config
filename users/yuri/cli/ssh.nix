8505381680e6e33d06df5d25f2b6fbf16ceebcb3
Yuri <hi@yuri.li>
2026-01-15 09:57:47 +0100

    Add hosts to SSH config

--- users/yuri/cli/ssh.nix
+++ users/yuri/cli/ssh.nix
@@ -9,51 +9,65 @@
     programs.ssh = {
       enable = true;
       enableDefaultConfig = false;
-      matchBlocks."catboy-house" = {
-        hostname = "46.4.241.139";
-        user = "yuri";
-        port = 29158;
-        identityFile = "${config.home.homeDirectory}/.ssh/id_catboy-house";
-      };
-      matchBlocks."*.repo.borgbase.com" = {
-        hostname = "%h";
-        identityFile = "${config.home.homeDirectory}/.ssh/id_borgbase";
-      };
-      matchBlocks."mantis" = {
-        hostname = "192.168.0.11";
-        user = "yuri";
-        port = 20615;
-        identityFile = "${config.home.homeDirectory}/.ssh/id_mantis";
-      };
-      matchBlocks."github.com" = {
-        user = "git";
-        identityFile = "${config.home.homeDirectory}/.ssh/id_github";
-      };
-      matchBlocks."gitlab.com" = {
-        user = "git";
-        identityFile = "${config.home.homeDirectory}/.ssh/id_gitlab";
-      };
-      matchBlocks."gitlab.alt.coop" = {
-        identityFile = "${config.home.homeDirectory}/.ssh/id_gitlab_alt_coop";
-      };
-      matchBlocks."ocelot" = {
-        hostname = "192.168.0.10";
-        user = "root";
-        port = 20141;
-        identityFile = "${config.home.homeDirectory}/.ssh/id_ocelot";
-      };
-      matchBlocks."otacon" = {
-        hostname = "192.168.0.242";
-        user = "yuri";
-        port = 21896;
-        identityFile = "${config.home.homeDirectory}/.ssh/id_otacon";
-      };
-
-      matchBlocks."*" = {
-        identitiesOnly = true;
-        addKeysToAgent = lib.mkIf pkgs.stdenv.isDarwin "yes";
-        extraOptions = {
-          UseKeychain = lib.mkIf pkgs.stdenv.isDarwin "yes";
+      matchBlocks = {
+        "catboy-house" = {
+          hostname = "46.4.241.139";
+          user = "yuri";
+          port = 29158;
+          identityFile = "${config.home.homeDirectory}/.ssh/id_catboy-house";
+        };
+        "*.repo.borgbase.com" = {
+          hostname = "%h";
+          identityFile = "${config.home.homeDirectory}/.ssh/id_borgbase";
+        };
+        "mantis" = {
+          hostname = "192.168.0.11";
+          user = "yuri";
+          port = 20615;
+          identityFile = "${config.home.homeDirectory}/.ssh/id_mantis";
+        };
+        "github.com" = {
+          user = "git";
+          identityFile = "${config.home.homeDirectory}/.ssh/id_github";
+        };
+        "gitlab.com" = {
+          user = "git";
+          identityFile = "${config.home.homeDirectory}/.ssh/id_gitlab";
+        };
+        "gitlab.alt.coop" = {
+          identityFile = "${config.home.homeDirectory}/.ssh/id_gitlab_alt_coop";
+        };
+        "git.alt.coop" = {
+          identityFile = "${config.home.homeDirectory}/.ssh/id_git_alt_coop";
+        };
+        "ocelot" = {
+          hostname = "192.168.0.10";
+          user = "root";
+          port = 20141;
+          identityFile = "${config.home.homeDirectory}/.ssh/id_ocelot";
+        };
+        "otacon" = {
+          hostname = "192.168.0.242";
+          user = "yuri";
+          port = 21896;
+          identityFile = "${config.home.homeDirectory}/.ssh/id_otacon";
+        };
+        "hamilton" = {
+          hostname = "hamilton.alt.coop";
+          user = "yuri";
+          identityFile = "${config.home.homeDirectory}/.ssh/id_hamilton";
+        };
+        "wagner" = {
+          hostname = "wagner.alt.coop";
+          user = "yuri";
+          identityFile = "${config.home.homeDirectory}/.ssh/id_wagner";
+        };
+        "*" = {
+          identitiesOnly = true;
+          addKeysToAgent = lib.mkIf pkgs.stdenv.isDarwin "yes";
+          extraOptions = {
+            UseKeychain = lib.mkIf pkgs.stdenv.isDarwin "yes";
+          };
         };
       };
     };
