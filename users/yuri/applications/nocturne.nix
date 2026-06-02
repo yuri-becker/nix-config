ee9ba28ab3774fe1b8e11a6390fd6eac841e2806
Yuri <hi@yuri.li>
2026-06-02 10:00:46 +0200

    Use Nocturne from nixpkgs

--- users/yuri/applications/nocturne.nix
+++ users/yuri/applications/nocturne.nix
@@ -6,29 +6,29 @@
 }:
 {
   config = lib.mkIf (config.localhost.enable && pkgs.stdenv.isLinux) {
-    services.flatpak = {
-      enable = lib.mkDefault true;
-      packages = [ "com.jeffser.Nocturne" ];
-    };
+    home.packages = with pkgs; [ nocturne ];
 
-    # home.file.".var/app/com.jeffser.Nocturne/config/glib-2.0/settings/keyfile".text = ''
-    #   [com/jeffser/Nocturne]
-    #   integration-library-dir='${config.home.homeDirectory}/Music'
-    #   selected-instance-type='NocturneIntegrationNavidrome'
-    #   integration-ip='https://music.catboy.house'
-    #   integration-user='yuri'
-    #   integration-trust-server=false
-    #   volume=1.0
-    #   default-page-tag='home'
-    #   visualizer-manual-color='0.11,0.44,0.85'
-    #   player-dynamic-bg-mode='blur'
-    #   show-visualizer=true
-    #   visualizer-type='wave'
-    #   default-width=1152
-    #   default-height=682
-    #   use-big-footer=true
-    #   player-blur-bg=false
-    #   auto-play=false
-    # '';
+    dconf.settings."com/jeffser/Nocturne" = {
+      button-size = "big";
+      default-page-tag = "home";
+      global-dynamic-bg-mode = "gradient";
+      integration-ip = "https://music.catboy.house";
+      integration-library-dir = "${config.home.homeDirectory}/Music";
+      integration-trust-server = false;
+      integration-user = "yuri";
+      max-bitrate = 0;
+      selected-instance-type = "NocturneIntegrationNavidrome";
+      show-carousel-pan-buttons = false;
+      show-visualizer = true;
+      sidebar-disabled-pages = [
+        "albums-starred"
+        "albums-frequent"
+      ];
+      use-sidebar-player = true;
+      visualizer-auto-color = true;
+      visualizer-bar-n = 12;
+      visualizer-fill-mode = "translucent";
+      visualizer-type = "wave";
+    };
   };
 }
